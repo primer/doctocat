9b09682fdb86e9526ed6a5178654d271c8e46b6c
Cole Bemis <colebemis@github.com>
2019-06-28 14:57:36 -0700

    Add copy to clipboard button to code blocks

--- packages/gatsby-theme-primer/src/components/code.js
+++ packages/gatsby-theme-primer/src/components/code.js
@@ -1,36 +1,42 @@
-import {BorderBox, Text} from '@primer/components'
+import {BorderBox, Text, Relative, Absolute, Flex} from '@primer/components'
 import Highlight, {defaultProps} from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/github'
 import React from 'react'
+import ClipboardCopy from './clipboard-copy'
 
 function Code({className, children}) {
   const language = className ? className.replace(/language-/, '') : null
   return (
-    <Highlight
-      {...defaultProps}
-      code={children.trim()}
-      language={language}
-      theme={theme}
-    >
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <BorderBox
-          as="pre"
-          className={className}
-          style={{...style, overflow: 'auto'}}
-          border={0}
-        >
-          <Text display="inline-block" p={3} fontFamily="mono" fontSize={1}>
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({line, key: i})}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({token, key})} />
-                ))}
-              </div>
-            ))}
-          </Text>
-        </BorderBox>
-      )}
-    </Highlight>
+    <Relative>
+      <Absolute top={8} right={8}>
+        <ClipboardCopy value={children} />
+      </Absolute>
+      <Highlight
+        {...defaultProps}
+        code={children.trim()}
+        language={language}
+        theme={theme}
+      >
+        {({className, style, tokens, getLineProps, getTokenProps}) => (
+          <BorderBox
+            as="pre"
+            className={className}
+            style={{...style, overflow: 'auto'}}
+            border={0}
+          >
+            <Text display="inline-block" p={3} fontFamily="mono" fontSize={1}>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({line, key: i})}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({token, key})} />
+                  ))}
+                </div>
+              ))}
+            </Text>
+          </BorderBox>
+        )}
+      </Highlight>
+    </Relative>
   )
 }
 
