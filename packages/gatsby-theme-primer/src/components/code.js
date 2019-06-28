b52461961dc55b45219a085edd90bdec380ae66b
Cole Bemis <colebemis@github.com>
2019-06-28 15:49:39 -0700

    Implement live editor

--- packages/gatsby-theme-primer/src/components/code.js
+++ packages/gatsby-theme-primer/src/components/code.js
@@ -1,11 +1,38 @@
-import {Absolute, BorderBox, Relative, Text} from '@primer/components'
+import {Absolute, BorderBox, Box, Relative, Text} from '@primer/components'
 import Highlight, {defaultProps} from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/github'
 import React from 'react'
+import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live'
 import ClipboardCopy from './clipboard-copy'
 
-function Code({className, children}) {
+function Code({className, children, live}) {
   const language = className ? className.replace(/language-/, '') : null
+
+  if (live) {
+    return (
+      <BorderBox mb={3} css={{overflow: 'hidden'}}>
+        <LiveProvider code={children.trim()}>
+          <Box as={LivePreview} p={3} />
+          <Relative>
+            <LiveEditor theme={theme} />
+            <Absolute top={8} right={8}>
+              <ClipboardCopy value={children} />
+            </Absolute>
+          </Relative>
+          <Text
+            as={LiveError}
+            m={0}
+            p={3}
+            fontFamily="mono"
+            fontSize={1}
+            color="white"
+            bg="red.6"
+          />
+        </LiveProvider>
+      </BorderBox>
+    )
+  }
+
   return (
     <Relative>
       <Absolute top={0} right={0} p={2}>
@@ -22,7 +49,6 @@ function Code({className, children}) {
             as="pre"
             className={className}
             style={{...style, overflow: 'auto'}}
-            border={0}
           >
             <Text display="inline-block" p={3} fontFamily="mono" fontSize={1}>
               {tokens.map((line, i) => (
