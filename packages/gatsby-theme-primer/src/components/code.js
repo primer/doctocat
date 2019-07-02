1860e92185a4512878104530ce1c5c02f1b259c6
Cole Bemis <colebemis@github.com>
2019-07-02 09:06:00 -0700

    Create LiveCode component

--- packages/gatsby-theme-primer/src/components/code.js
+++ packages/gatsby-theme-primer/src/components/code.js
@@ -1,58 +1,26 @@
-import {Absolute, BorderBox, Box, Relative, Text} from '@primer/components'
+import {Absolute, BorderBox, Relative, Text} from '@primer/components'
 import Highlight, {defaultProps} from 'prism-react-renderer'
 import githubTheme from 'prism-react-renderer/themes/github'
-import React, {useContext} from 'react'
-import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live'
-import {ThemeContext} from 'styled-components'
+import React from 'react'
 import ClipboardCopy from './clipboard-copy'
+import LiveCode from './live-code'
 
 function Code({className, children, live}) {
-  const theme = useContext(ThemeContext)
-  const language = className ? className.replace(/language-/, '') : null
+  const language = className ? className.replace(/language-/, '') : ''
+  const code = children.trim()
 
   if (live) {
-    return (
-      <BorderBox mb={3} css={{overflow: 'hidden'}}>
-        <LiveProvider code={children.trim()}>
-          <Box p={3}>
-            <LivePreview />
-          </Box>
-          <Relative>
-            <LiveEditor
-              theme={githubTheme}
-              ignoreTabKey={true}
-              padding={theme.space[3]}
-              style={{
-                fontFamily: theme.fonts.mono,
-                fontSize: theme.fontSizes[1],
-              }}
-            />
-            <Absolute top={8} right={8}>
-              <ClipboardCopy value={children} />
-            </Absolute>
-          </Relative>
-          <Text
-            as={LiveError}
-            m={0}
-            p={3}
-            fontFamily="mono"
-            fontSize={1}
-            color="white"
-            bg="red.5"
-          />
-        </LiveProvider>
-      </BorderBox>
-    )
+    return <LiveCode code={code} />
   }
 
   return (
     <Relative>
       <Absolute top={0} right={0} p={2}>
-        <ClipboardCopy value={children} />
+        <ClipboardCopy value={code} />
       </Absolute>
       <Highlight
         {...defaultProps}
-        code={children.trim()}
+        code={code}
         language={language}
         theme={githubTheme}
       >
