b890332589eedc82fe1ad5f894ee78ad34173416
Cole Bemis <colebemis@github.com>
2019-08-03 16:11:18 -0700

    Use react-measure for sticky sidebar

--- theme/src/components/sidebar.js
+++ theme/src/components/sidebar.js
@@ -1,47 +1,39 @@
 import {BorderBox, Flex, Position} from '@primer/components'
 import React from 'react'
+import Measure from 'react-measure'
 import navItems from '../nav.yml'
 import NavItems from './nav-items'
 
 function Sidebar() {
-  const rootElement = React.useRef(null)
-  const {top = 0} = useBoundingClientRect(rootElement)
+  const [top, setTop] = React.useState(0)
 
   return (
-    <Position
-      ref={rootElement}
-      position="sticky"
-      top={top}
-      height={`calc(100vh - ${top}px)`}
-      minWidth={280}
-      color="gray.8"
-      bg="gray.0"
-    >
-      <BorderBox
-        border={0}
-        borderRight={1}
-        borderRadius={0}
-        height="100%"
-        style={{overflow: 'auto'}}
-      >
-        <Flex flexDirection="column" pt={3} pb={4}>
-          <NavItems items={navItems} />
-        </Flex>
-      </BorderBox>
-    </Position>
+    <Measure bounds={true} onResize={rect => setTop(rect.bounds.top)}>
+      {({measureRef}) => (
+        <Position
+          ref={measureRef}
+          position="sticky"
+          top={top}
+          height={`calc(100vh - ${top}px)`}
+          minWidth={280}
+          color="gray.8"
+          bg="gray.0"
+        >
+          <BorderBox
+            border={0}
+            borderRight={1}
+            borderRadius={0}
+            height="100%"
+            style={{overflow: 'auto'}}
+          >
+            <Flex flexDirection="column" pt={3} pb={4}>
+              <NavItems items={navItems} />
+            </Flex>
+          </BorderBox>
+        </Position>
+      )}
+    </Measure>
   )
-}
-
-function useBoundingClientRect(ref) {
-  const [boundingClientRect, setBoundingClientRect] = React.useState({})
-
-  React.useEffect(() => {
-    if (ref.current) {
-      setBoundingClientRect(ref.current.getBoundingClientRect())
-    }
-  })
-
-  return boundingClientRect
 }
 
 export default Sidebar
