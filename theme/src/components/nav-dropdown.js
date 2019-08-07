d103a03c2c839990e0d4028b70cdbba4b631d04c
Cole Bemis <colebemis@github.com>
2019-08-07 15:14:13 -0700

    Fix nav dropdown links

--- theme/src/components/nav-dropdown.js
+++ theme/src/components/nav-dropdown.js
@@ -1,7 +1,6 @@
 import {
   Absolute,
   BorderBox,
-  Details,
   StyledOcticon,
   Text,
   themeGet,
@@ -10,28 +9,49 @@ import {ChevronDown} from '@primer/octicons-react'
 import React from 'react'
 import styled from 'styled-components'
 
+// TODO: Replace this Details component with the one from @primer/components when 14.0.0 is released.
+// Reference: https://github.com/primer/components/pull/499
+const Details = styled.details`
+  & > summary {
+    list-style: none;
+  }
+
+  & > summary::-webkit-details-marker {
+    display: none;
+  }
+`
+
 function NavDropdown({title, children}) {
+  const [open, setOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    if (open) {
+      document.addEventListener('click', closeMenu)
+      return () => {
+        document.removeEventListener('click', closeMenu)
+      }
+    }
+  }, [open])
+
+  function toggle(event) {
+    setOpen(event.target.open)
+  }
+
+  function closeMenu() {
+    setOpen(false)
+  }
+
   return (
-    <Details overlay={true}>
-      {({toggle}) => (
-        <>
-          <summary onClick={toggle} style={{cursor: 'pointer'}}>
-            <Text>{title}</Text>
-            <StyledOcticon icon={ChevronDown} ml={1} />
-          </summary>
-          <Absolute>
-            <BorderBox
-              bg="white"
-              py={1}
-              mt={2}
-              boxShadow="medium"
-              color="gray.8"
-            >
-              {children}
-            </BorderBox>
-          </Absolute>
-        </>
-      )}
+    <Details open={open} onToggle={toggle}>
+      <summary style={{cursor: 'pointer'}}>
+        <Text>{title}</Text>
+        <StyledOcticon icon={ChevronDown} ml={1} />
+      </summary>
+      <Absolute>
+        <BorderBox bg="white" py={1} mt={2} boxShadow="medium" color="gray.8">
+          {children}
+        </BorderBox>
+      </Absolute>
     </Details>
   )
 }
