cb562e32204dd81c667bf97166fe1b64a03f101f
Cole Bemis <colebemis@github.com>
2019-08-14 13:16:48 -0700

    Remove "Table of contents" heading

--- theme/src/components/table-of-contents.js
+++ theme/src/components/table-of-contents.js
@@ -2,17 +2,6 @@ import {MDXContext} from '@mdx-js/react'
 import React from 'react'
 
 function TableOfContents({items}) {
-  const {h2: H2 = 'h2'} = React.useContext(MDXContext)
-
-  return (
-    <>
-      <H2>Table of contents</H2>
-      <TableOfContentsItems items={items} />
-    </>
-  )
-}
-
-function TableOfContentsItems({items}) {
   const {
     ul: List = 'ul',
     li: ListItem = 'li',
@@ -24,7 +13,7 @@ function TableOfContentsItems({items}) {
       {items.map(item => (
         <ListItem key={item.url}>
           <Link href={item.url}>{item.title}</Link>
-          {item.items ? <TableOfContentsItems items={item.items} /> : null}
+          {item.items ? <TableOfContents items={item.items} /> : null}
         </ListItem>
       ))}
     </List>
