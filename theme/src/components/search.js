29b5f2544891d9e9a5ab9d4a97d054acef257f93
Cole Bemis <colebemis@github.com>
2019-08-22 15:35:09 -0700

    Update state management in search component

--- theme/src/components/search.js
+++ theme/src/components/search.js
@@ -7,6 +7,19 @@ import useSiteMetadata from '../use-site-metadata'
 import DarkTextInput from './dark-text-input'
 import SearchResults from './search-results'
 
+function stateReducer(state, changes) {
+  switch (changes.type) {
+    case Downshift.stateChangeTypes.changeInput:
+      if (!changes.inputValue) {
+        // Close the menu if the input is empty.
+        return {...changes, isOpen: false}
+      }
+      return changes
+    default:
+      return changes
+  }
+}
+
 function Search() {
   const [query, setQuery] = React.useState('')
   const results = useSearch(query)
@@ -16,13 +29,19 @@ function Search() {
     <Downshift
       inputValue={query}
       onInputValueChange={inputValue => setQuery(inputValue)}
+      // We don't need Downshift to keep track of a selected item because as
+      // soon as an item is selected we navigate to a new page.
+      // Let's avoid any unexpected states related to the selected item
+      // by setting it to always be `null`.
+      selectedItem={null}
       onSelect={item => {
         if (item) {
-          setQuery('')
           navigate(item.path)
+          setQuery('')
         }
       }}
       itemToString={item => (item ? item.title : '')}
+      stateReducer={stateReducer}
     >
       {({
         getInputProps,
@@ -30,19 +49,16 @@ function Search() {
         getMenuProps,
         getRootProps,
         isOpen,
-        inputValue,
         highlightedIndex,
-        clearSelection,
       }) => (
         <Position {...getRootProps({position: 'relative'})}>
           <DarkTextInput
             {...getInputProps({
               placeholder: `Search ${siteMetadata.title}`,
               width: 240,
-              onChange: () => clearSelection(),
             })}
           />
-          {isOpen && inputValue ? (
+          {isOpen ? (
             <Position
               {...getMenuProps({
                 position: 'absolute',
