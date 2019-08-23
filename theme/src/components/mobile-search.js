86d9b37394045675e88751589c323de408eae19e
Cole Bemis <colebemis@github.com>
2019-08-22 17:36:32 -0700

    Animate mobile search enter/exit

--- theme/src/components/mobile-search.js
+++ theme/src/components/mobile-search.js
@@ -1,13 +1,14 @@
-import {Absolute, Box, Fixed, Flex, StyledOcticon} from '@primer/components'
+import {Absolute, Fixed, Flex, StyledOcticon} from '@primer/components'
 import {X} from '@primer/octicons-react'
 import Downshift from 'downshift'
+import {AnimatePresence, motion} from 'framer-motion'
+import {navigate} from 'gatsby'
 import React from 'react'
+import {FocusOn} from 'react-focus-on'
+import useSearch from '../use-search'
 import DarkButton from './dark-button'
 import DarkTextInput from './dark-text-input'
-import useSearch from '../use-search'
 import SearchResults from './search-results'
-import {navigate} from 'gatsby'
-import {FocusOn} from 'react-focus-on'
 
 function stateReducer(state, changes) {
   switch (changes.type) {
@@ -34,87 +35,109 @@ function MobileSearch({isOpen, onDismiss}) {
     onDismiss()
   }
 
-  return isOpen ? (
-    <FocusOn returnFocus={true}>
-      <Fixed top={0} left={0} right={0} bottom={0} zIndex={1}>
-        <Absolute
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          bg="rgba(0,0,0,0.5)"
-          zIndex={-1}
-          onClick={handleDismiss}
-        />
-        <Flex flexDirection="column" height="100%">
-          <Downshift
-            inputValue={query}
-            onInputValueChange={inputValue => setQuery(inputValue)}
-            selectedItem={null}
-            onSelect={item => {
-              if (item) {
-                navigate(item.path)
-                setQuery('')
-                onDismiss()
-              }
-            }}
-            itemToString={item => (item ? item.title : '')}
-            stateReducer={stateReducer}
-          >
-            {({
-              getInputProps,
-              getItemProps,
-              getMenuProps,
-              getRootProps,
-              isOpen: isMenuOpen,
-              highlightedIndex,
-            }) => (
-              <Flex
-                {...getRootProps({flexDirection: 'column', height: '100%'})}
+  return (
+    <AnimatePresence>
+      {isOpen ? (
+        <FocusOn returnFocus={true}>
+          <Fixed top={0} left={0} right={0} bottom={0} zIndex={1}>
+            <Absolute
+              as={motion.div}
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              exit={{opacity: 0}}
+              transition={{type: 'tween'}}
+              top={0}
+              left={0}
+              right={0}
+              bottom={0}
+              bg="rgba(0,0,0,0.5)"
+              zIndex={-1}
+              onClick={handleDismiss}
+            />
+            <Flex flexDirection="column" height="100%">
+              <Downshift
+                inputValue={query}
+                onInputValueChange={inputValue => setQuery(inputValue)}
+                selectedItem={null}
+                onSelect={item => {
+                  if (item) {
+                    navigate(item.path)
+                    setQuery('')
+                    onDismiss()
+                  }
+                }}
+                itemToString={item => (item ? item.title : '')}
+                stateReducer={stateReducer}
               >
-                <Flex bg="gray.9" color="white" p={3}>
-                  <DarkTextInput
-                    {...getInputProps({
-                      autoFocus: true,
-                      placeholder: `Search`,
-                      width: '100%',
-                    })}
-                  />
-                  <DarkButton
-                    ml={3}
-                    aria-label="Cancel"
-                    onClick={handleDismiss}
-                  >
-                    <StyledOcticon icon={X} />
-                  </DarkButton>
-                </Flex>
-                {isMenuOpen ? (
+                {({
+                  getInputProps,
+                  getItemProps,
+                  getMenuProps,
+                  getRootProps,
+                  isOpen: isMenuOpen,
+                  highlightedIndex,
+                }) => (
                   <Flex
-                    {...getMenuProps({
-                      bg: 'white',
-                      py: 1,
-                      flexDirection: 'column',
-                      flex: '1 1 auto',
-                      style: {
-                        overflow: 'auto',
-                        WebkitOverflowScrolling: 'touch',
-                      },
-                    })}
+                    {...getRootProps({flexDirection: 'column', height: '100%'})}
                   >
-                    <SearchResults
-                      results={results}
-                      getItemProps={getItemProps}
-                      highlightedIndex={highlightedIndex}
-                    />
+                    <Flex
+                      as={motion.div}
+                      initial={{x: '25%', opacity: 0}}
+                      animate={{x: 0, opacity: 1}}
+                      exit={{x: '25%', opacity: 0}}
+                      transition={{type: 'tween', duration: 0.2}}
+                      bg="gray.9"
+                      color="white"
+                      p={3}
+                    >
+                      <DarkTextInput
+                        {...getInputProps({
+                          autoFocus: true,
+                          placeholder: `Search`,
+                          width: '100%',
+                        })}
+                      />
+                      <DarkButton
+                        ml={3}
+                        aria-label="Cancel"
+                        onClick={handleDismiss}
+                      >
+                        <StyledOcticon icon={X} />
+                      </DarkButton>
+                    </Flex>
+                    {isMenuOpen ? (
+                      <Flex
+                        {...getMenuProps({
+                          as: motion.div,
+                          animate: {opacity: 1},
+                          exit: {opacity: 0},
+                          transition: {type: 'tween'},
+                          bg: 'white',
+                          py: 1,
+                          flexDirection: 'column',
+                          flex: '1 1 auto',
+                          style: {
+                            overflow: 'auto',
+                            WebkitOverflowScrolling: 'touch',
+                          },
+                        })}
+                      >
+                        <SearchResults
+                          results={results}
+                          getItemProps={getItemProps}
+                          highlightedIndex={highlightedIndex}
+                        />
+                      </Flex>
+                    ) : null}
                   </Flex>
-                ) : null}
-              </Flex>
-            )}
-          </Downshift>
-        </Flex>
-      </Fixed>
-    </FocusOn>
-  ) : null
+                )}
+              </Downshift>
+            </Flex>
+          </Fixed>
+        </FocusOn>
+      ) : null}
+    </AnimatePresence>
+  )
 }
 
 export default MobileSearch
