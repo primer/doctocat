14bc6b9ee44202be8ad0f1bb16e0fc5ebcc0df50
Emily Plummer <emplums@github.com>
2019-08-26 16:46:22 -0700

    pass props

--- theme/src/components/caption.js
+++ theme/src/components/caption.js
@@ -1,6 +1,6 @@
 import {Text} from '@primer/components'
 import React from 'react'
 
-const Caption = ({children}) => <Text fontSize="0" color="gray.5">{children}</Text>
+const Caption = (props) => <Text fontSize="0" color="gray.5" {...props}/>
 
 export default Caption
