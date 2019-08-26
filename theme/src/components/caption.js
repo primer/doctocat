521b49d5f560ca3d536714c62bc3b0497512bf2f
Emily Plummer <emplums@github.com>
2019-08-26 16:51:51 -0700

    use p tag

--- theme/src/components/caption.js
+++ theme/src/components/caption.js
@@ -1,6 +1,6 @@
 import {Text} from '@primer/components'
 import React from 'react'
 
-const Caption = (props) => <Text fontSize="1" color="gray.5" {...props}/>
+const Caption = (props) => <Text as='p' fontSize='1' color='gray.5' {...props}/>
 
 export default Caption
