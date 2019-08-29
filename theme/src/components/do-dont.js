99a288a40357ab6cfb314cb91ce3cdabf48ab0e5
Cole Bemis <colebemis@github.com>
2019-08-29 16:28:41 -0700

    Update do/dont components

--- theme/src/components/do-dont.js
+++ theme/src/components/do-dont.js
@@ -1,34 +1,48 @@
+import {CircleOcticon, Flex, Text} from '@primer/components'
+import {Check, X} from '@primer/octicons-react'
 import React from 'react'
-import {Flex, CircleOcticon, Text, Box} from '@primer/components'
 import Caption from './caption'
-import {X, Check} from '@primer/octicons-react'
 
-export const Do = ({src, children}) => {
+export function Do({src, children}) {
   return (
-    <Flex flexDirection='column' mr="2">
-      <Flex alignSelf='start' flexDirection="row" alignItems="center" mb="2">
-        <CircleOcticon icon={Check} size={16} bg="green.5" color="white" mr="2" p="1"/>
+    <Flex flexDirection="column">
+      <Flex alignSelf="start" flexDirection="row" alignItems="center" mb="2">
+        <CircleOcticon
+          icon={Check}
+          size={16}
+          bg="green.5"
+          color="white"
+          mr="2"
+          p="1"
+        />
         <Text fontWeight="bold" color="gray.9" ml="1">
           Do
         </Text>
       </Flex>
-      <Box as='img' src={src} width="100%" height="auto"/>
-      <Caption {...children}/>
+      <img src={src} width="100%" />
+      <Caption>{children}</Caption>
     </Flex>
   )
 }
 
-export const Dont = ({src, children}) => {
+export function Dont({src, children}) {
   return (
-    <Flex flexDirection='column' mr="2">
-      <Flex alignSelf='start' flexDirection="row" alignItems="center" mb="2">
-        <CircleOcticon icon={X} size={16} bg="red.5" color="white" mr="2" p="1"/>
+    <Flex flexDirection="column">
+      <Flex alignSelf="start" flexDirection="row" alignItems="center" mb="2">
+        <CircleOcticon
+          icon={X}
+          size={16}
+          bg="red.5"
+          color="white"
+          mr="2"
+          p="1"
+        />
         <Text fontWeight="bold" color="gray.9" ml="1">
           Don't
         </Text>
       </Flex>
-      <Box as='img' src={src} width="100%" height="auto"/>
-      <Caption {...children}/>
+      <img src={src} width="100%" />
+      <Caption>{children}</Caption>
     </Flex>
   )
 }
