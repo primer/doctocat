75dcbfe5f70ce63caf1992d149561d23a6588aec
Cole Bemis <colebemis@github.com>
2019-09-04 16:16:48 -0700

    Add text label

--- theme/src/components/source-link.js
+++ theme/src/components/source-link.js
@@ -1,19 +1,13 @@
-import {Link, Tooltip} from '@primer/components'
-import Octicon, {FileCode} from '@primer/octicons-react'
+import {Link, StyledOcticon} from '@primer/components'
+import {FileCode} from '@primer/octicons-react'
 import React from 'react'
 
 function SourceLink({href}) {
   return (
-    <Tooltip aria-label="View source" direction="nw">
-      <Link
-        href={href}
-        aria-label="View source"
-        p={2}
-        lineHeight="condensedUltra"
-      >
-        <Octicon icon={FileCode} />
-      </Link>
-    </Tooltip>
+    <Link href={href} lineHeight="condensedUltra" fontSize={1}>
+      <StyledOcticon icon={FileCode} mr={2} />
+      View source
+    </Link>
   )
 }
 
