452a2305e6549ab02a76ccf3e9876908c4f620b8
Derrick Marcey <demarcey@microsoft.com>
2019-09-05 12:02:45 -0400

    Replace \ with / in page paths to get Doctocat working on Windows.(#75)
    * Replace \ with / in page paths to get Doctocat working on Windows.
    
    * PR Feedback - update code comment.

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -46,10 +46,12 @@ exports.createPages = async ({graphql, actions}, themeOptions) => {
   // Turn every MDX file into a page.
   return Promise.all(
     data.allMdx.nodes.map(async node => {
-      const pagePath = path.join(
-        node.parent.relativeDirectory,
-        node.parent.name === 'index' ? '/' : node.parent.name,
-      )
+      const pagePath = path
+        .join(
+          node.parent.relativeDirectory,
+          node.parent.name === 'index' ? '/' : node.parent.name,
+        )
+        .replace(/\\/g, '/') // Convert Windows backslash paths to forward slash paths: foo\\bar → foo/bar
 
       const rootAbsolutePath = path.resolve(
         process.cwd(),
