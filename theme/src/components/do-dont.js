4b19d09045dd070327bfc518644106df9612fedb
Derrick Marcey <demarcey@microsoft.com>
2019-09-06 12:26:10 -0400

    Refactored common Do / Dont code into DoDontBase.

--- theme/src/components/do-dont.js
+++ theme/src/components/do-dont.js
@@ -19,46 +19,36 @@ DoDont.defaultProps = {
   stacked: false,
 }
 
-export function Do({src, alt, children}) {
+export function Do(props) {
+  return <DoDontBase {...props} text="Do" icon={Check} iconBg="green.5" />
+}
+
+export function Dont(props) {
+  return <DoDontBase {...props} text="Don't" icon={X} iconBg="red.5" />
+}
+
+function DoDontBase({src, alt, children, text, icon, iconBg}) {
   return (
     <Flex flexDirection="column">
       <Flex alignSelf="start" flexDirection="row" alignItems="center" mb="2">
         <CircleOcticon
-          icon={Check}
+          icon={icon}
           size={16}
-          bg="green.5"
+          bg={iconBg}
           color="white"
           mr="2"
           p="1"
         />
         <Text fontWeight="bold" color="gray.9" ml="1">
-          Do
+          {text}
         </Text>
       </Flex>
-      <img src={src} alt={alt || ''} width="100%" />
+      <img src={src} alt={alt} width="100%" />
       <Caption mb={0}>{children}</Caption>
     </Flex>
   )
 }
 
-export function Dont({src, alt, children}) {
-  return (
-    <Flex flexDirection="column">
-      <Flex alignSelf="start" flexDirection="row" alignItems="center" mb="2">
-        <CircleOcticon
-          icon={X}
-          size={16}
-          bg="red.5"
-          color="white"
-          mr="2"
-          p="1"
-        />
-        <Text fontWeight="bold" color="gray.9" ml="1">
-          Don't
-        </Text>
-      </Flex>
-      <img src={src} alt={alt || ''} width="100%" />
-      <Caption mb={0}>{children}</Caption>
-    </Flex>
-  )
+DoDontBase.defaultProps = {
+  alt: '',
 }
