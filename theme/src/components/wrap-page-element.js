ba941ff500eead49986d17213fc8ecd2aefd3d3e
Derrick Marcey <demarcey@microsoft.com>
2019-09-13 07:59:01 -0400

    Adding SkipNavLink to page element.

--- theme/src/components/wrap-page-element.js
+++ theme/src/components/wrap-page-element.js
@@ -1,8 +1,14 @@
 import {BaseStyles} from '@primer/components'
+import SkipNavLink from './skip-nav-link'
 import React from 'react'
 
 function wrapPageElement({element}) {
-  return <BaseStyles>{element}</BaseStyles>
+  return (
+    <BaseStyles>
+      <SkipNavLink />
+      {element}
+    </BaseStyles>
+  )
 }
 
 export default wrapPageElement
