a74261a84d18995c0ae720e9c0aabfc6abb40650
Emily Plummer <emplums@github.com>
2020-02-21 15:30:24 -0800

    remove box shadow from dark button

--- theme/src/components/dark-button.js
+++ theme/src/components/dark-button.js
@@ -5,6 +5,7 @@ const DarkButton = styled(ButtonOutline)`
   color: ${themeGet('colors.blue.2')};
   background-color: transparent;
   border: 1px solid ${themeGet('colors.gray.7')};
+  box-shadow: none;
 `
 
 export default DarkButton
