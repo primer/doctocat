7571c9699417a13b0019095ab509f10beece2364
Cole Bemis <colebemis@github.com>
2020-03-31 22:27:39 -0700

    Update toc list item padding

--- theme/src/components/table-of-contents.js
+++ theme/src/components/table-of-contents.js
@@ -5,7 +5,7 @@ function TableOfContents({items, depth}) {
   return (
     <Box as="ul" m={0} p={0} css={{listStyle: 'none'}}>
       {items.map(item => (
-        <Box as="li" key={item.url} pl={depth * 16}>
+        <Box as="li" key={item.url} pl={depth > 0 ? 3 : 0}>
           <Link display="inline-block" py={1} href={item.url} color="gray.6">
             {item.title}
           </Link>
