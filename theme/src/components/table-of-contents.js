e2c1c7d04ebfd9e674d6e0e564aed00d93154db3
Cole Bemis <colebemis@github.com>
2020-08-25 10:29:11 -0700

    Reduce font size of table of contents items

--- theme/src/components/table-of-contents.js
+++ theme/src/components/table-of-contents.js
@@ -4,9 +4,15 @@ import React from 'react'
 function TableOfContents({items, depth}) {
   return (
     <Box as="ul" m={0} p={0} css={{listStyle: 'none'}}>
-      {items.map(item => (
+      {items.map((item) => (
         <Box as="li" key={item.url} pl={depth > 0 ? 3 : 0}>
-          <Link display="inline-block" py={1} href={item.url} color="gray.6">
+          <Link
+            display="inline-block"
+            py={1}
+            href={item.url}
+            fontSize={1}
+            color="gray.6"
+          >
             {item.title}
           </Link>
           {item.items ? (
