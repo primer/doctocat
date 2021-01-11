5c4268153b3fead1b731d2b75e01e09db2255f60
Cole Bemis <colebemis@github.com>
2021-01-11 14:32:22 -0800

    Style images inside ImageContainer

--- theme/src/components/image-container.js
+++ theme/src/components/image-container.js
@@ -4,7 +4,9 @@ import React from 'react'
 function ImageContainer({children}) {
   return (
     <BorderBox p={6} bg="gray.1">
-      <Flex justifyContent="center">{children}</Flex>
+      <Flex justifyContent="center" sx={{img: {maxWidth: '100%'}}}>
+        {children}
+      </Flex>
     </BorderBox>
   )
 }
