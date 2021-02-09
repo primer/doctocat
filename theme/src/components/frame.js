e6d4ebd1276338ea2d901ab4fdc99cad26513668
Ash Guillaume <10384315+ashygee@users.noreply.github.com>
2021-02-09 12:35:23 -0800

    add 6px border radius to FrameComponent

--- theme/src/components/frame.js
+++ theme/src/components/frame.js
@@ -6,7 +6,7 @@ import Measure from 'react-measure'
 function Frame({children}) {
   const [height, setHeight] = React.useState('auto')
   return (
-    <FrameComponent style={{width: '100%', border: 0, height}}>
+    <FrameComponent style={{width: '100%', border: 0, border-radius: '6px', height}}>
       <FrameContextConsumer>
         {({document}) => {
           // By default, styled-components injects styles in the head of the page.
