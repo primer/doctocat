23201f5f897b686bb4ddb80b5b7d0f323357a32c
Manuel Puyol <manuelpuyol@github.com>
2021-02-17 11:57:23 -0600

    use import instead of require

--- theme/src/prism.js
+++ theme/src/prism.js
@@ -1,31 +1,27 @@
 import Prism from 'prismjs/components/prism-core'
 
-// Add syntax highlighting support for ruby and erb
-// Reference: https://github.com/FormidableLabs/prism-react-renderer#faq
-(typeof global !== "undefined" ? global : window).Prism = Prism
-
 // Core languages
-require("prismjs/components/prism-clike");
-require("prismjs/components/prism-markup");
-require("prismjs/components/prism-markup-templating");
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
 
 // Supported languages
-require("prismjs/components/prism-bash");
-require("prismjs/components/prism-css");
-require("prismjs/components/prism-diff");
-require("prismjs/components/prism-go");
-require("prismjs/components/prism-json");
-require("prismjs/components/prism-markdown");
-require("prismjs/components/prism-yaml");
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-diff';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-yaml';
 
 // JS like
-require("prismjs/components/prism-javascript");
-require("prismjs/components/prism-jsx");
-require("prismjs/components/prism-typescript");
-require("prismjs/components/prism-tsx");
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-tsx';
 
 // Ruby like
-require("prismjs/components/prism-ruby");
-require("prismjs/components/prism-erb");
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-erb';
 
 export default Prism
