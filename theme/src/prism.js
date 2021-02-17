268b0123d8c34659c8b9f11f9b7faac0bac68ab0
Manuel Puyol <manuelpuyol@github.com>
2021-02-17 11:55:18 -0600

    correct require order

--- theme/src/prism.js
+++ theme/src/prism.js
@@ -11,14 +11,21 @@ require("prismjs/components/prism-markup-templating");
 
 // Supported languages
 require("prismjs/components/prism-bash");
+require("prismjs/components/prism-css");
 require("prismjs/components/prism-diff");
-require("prismjs/components/prism-erb");
-require("prismjs/components/prism-javascript");
+require("prismjs/components/prism-go");
 require("prismjs/components/prism-json");
-require("prismjs/components/prism-jsx");
 require("prismjs/components/prism-markdown");
-require("prismjs/components/prism-ruby");
 require("prismjs/components/prism-yaml");
 
+// JS like
+require("prismjs/components/prism-javascript");
+require("prismjs/components/prism-jsx");
+require("prismjs/components/prism-typescript");
+require("prismjs/components/prism-tsx");
+
+// Ruby like
+require("prismjs/components/prism-ruby");
+require("prismjs/components/prism-erb");
 
 export default Prism
