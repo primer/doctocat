86f46f0a9dfc9af2aed50dc483c6c9d6ffbbbab3
Manuel Puyol <manuelpuyol@github.com>
2021-02-17 11:48:40 -0600

    manually require languages

--- theme/src/prism.js
+++ theme/src/prism.js
@@ -3,6 +3,22 @@ import Prism from 'prismjs/components/prism-core'
 // Add syntax highlighting support for ruby and erb
 // Reference: https://github.com/FormidableLabs/prism-react-renderer#faq
 (typeof global !== "undefined" ? global : window).Prism = Prism
-require("prismjs/components/");
+
+// Core languages
+require("prismjs/components/prism-clike");
+require("prismjs/components/prism-markup");
+require("prismjs/components/prism-markup-templating");
+
+// Supported languages
+require("prismjs/components/prism-bash");
+require("prismjs/components/prism-diff");
+require("prismjs/components/prism-erb");
+require("prismjs/components/prism-javascript");
+require("prismjs/components/prism-json");
+require("prismjs/components/prism-jsx");
+require("prismjs/components/prism-markdown");
+require("prismjs/components/prism-ruby");
+require("prismjs/components/prism-yaml");
+
 
 export default Prism
