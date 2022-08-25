3b3d8f61773574832cb84eecbad6c1e0337ba30f
Josh Black <josh@josh.black>
2022-08-25 14:58:21 -0500

    feat(theme): support react 17, remove catch-links

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = themeOptions => {
       'gatsby-plugin-styled-components',
       'gatsby-plugin-react-helmet',
       'gatsby-plugin-remove-trailing-slashes',
-      'gatsby-plugin-catch-links',
       'gatsby-transformer-yaml',
       {
         resolve: 'gatsby-plugin-mdx',
