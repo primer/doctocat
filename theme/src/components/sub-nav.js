229df8708d1d3a0e58d7fea23b9f80b8681e8eb3
Josep Martins <josepmartins@github.com>
2022-09-20 20:52:22 +0200

    Sub-nav as component

---
+++ theme/src/components/sub-nav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {UnderlineNav} from '@primer/react'
+
+export function SubNav() {
+  return (
+    <UnderlineNav
+      aria-label="Page navigation"
+      sx={{position: 'sticky', top: '66px', background: '#ffffff', borderBottom: 'none'}}
+    >
+      <UnderlineNav.Link href="#usage" selected={window.location.hash === '#usage'}>
+        Usage
+      </UnderlineNav.Link>
+      <UnderlineNav.Link href="#styles" selected={window.location.hash === '#styles'}>
+        Styles
+      </UnderlineNav.Link>
+      <UnderlineNav.Link href="#react" selected={window.location.hash === '#react'}>
+        React
+      </UnderlineNav.Link>
+      <UnderlineNav.Link href="#rails" selected={window.location.hash === '#rails'}>
+        Rails
+      </UnderlineNav.Link>
+      <UnderlineNav.Link href="#accessibility" selected={window.location.hash === '#accessibility'}>
+        Accessibility
+      </UnderlineNav.Link>
+    </UnderlineNav>
+  )
+}
+
+export default SubNav
