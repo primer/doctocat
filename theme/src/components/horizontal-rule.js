c3e8a84771aa5e4997816ebe20b4b1fdbbe90c63
Josep Martins <josepmartins@github.com>
2022-10-04 16:22:57 +0200

    Use theme borderWidths

--- theme/src/components/horizontal-rule.js
+++ theme/src/components/horizontal-rule.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import themeGet from '@styled-system/theme-get'
 
 const HorizontalRule = styled.hr`
-  height: 1px;
+  height: ${themeGet('borderWidths.1')};
   padding: 0;
   margin: ${themeGet('space.4')} 0;
   background-color: ${themeGet('colors.border.default')};
