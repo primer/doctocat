1b86584da172b4709eaf07a1a5be6fd549a7b89a
Josep Martins <josepmartins@github.com>
2022-10-04 14:28:45 +0200

    Style footnotes section

--- theme/src/components/horizontal-rule.js
+++ theme/src/components/horizontal-rule.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import themeGet from '@styled-system/theme-get'
 
 const HorizontalRule = styled.hr`
-  height: ${themeGet('space.1')};
+  height: 1px;
   padding: 0;
   margin: ${themeGet('space.4')} 0;
   background-color: ${themeGet('colors.border.default')};
--- theme/src/components/superscript.js
+++ theme/src/components/superscript.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import {HEADER_HEIGHT} from './header'
 
 const Superscript = styled.sup`
   font-size: 75%;
@@ -6,6 +7,7 @@ const Superscript = styled.sup`
   position: relative;
   vertical-align: baseline;
   top: -0.5em;
+  scroll-margin-top: ${HEADER_HEIGHT + 24}px;
 
   a {
     text-decoration: none;
