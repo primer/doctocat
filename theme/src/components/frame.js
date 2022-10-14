6deef7001df5280934f9d050c8fa5557c0c21578
Orhan Toy <toyorhan@gmail.com>
2022-10-14 23:06:23 +0000

    Fix live examples disappearing on hard reload
    The version bump from 4.1.3 to 5.2.1 of react-frame-component seems to have introduced this issue:
    https://github.com/primer/doctocat/commit/a407024a74b82fc4b82f6e63de64ea6895cf5549
    
    There is an unresolved open issue https://github.com/ryanseddon/react-frame-component/pull/207 so I looked into if the dependency could be removed.

--- theme/src/components/frame.js
+++ theme/src/components/frame.js
@@ -1,34 +1,37 @@
 import React from 'react'
-import FrameComponent, {FrameContextConsumer} from 'react-frame-component'
+import ReactDOM from 'react-dom'
 import {StyleSheetManager} from 'styled-components'
 import Measure from 'react-measure'
 
 function Frame({children}) {
   const [height, setHeight] = React.useState('auto')
+  const [iframeRef, setIframeRef] = React.useState(null)
+  const contentDocument = iframeRef ? iframeRef.contentWindow.document : null
+
   return (
-    <FrameComponent style={{width: '100%', border: 0, borderRadius: 6, height}}>
-      <FrameContextConsumer>
-        {({document}) => {
-          // By default, styled-components injects styles in the head of the page.
-          // However, styles from the page's head don't apply inside iframes.
-          // We're using StyleSheetManager to make styled-components inject styles
-          // into the head of the iframe instead.
-          return (
-            <StyleSheetManager target={document.head}>
-              <Measure
-                // iframes don't adjust to the height of their content by default.
-                // We're using Measure to calculate the size of the content
-                // and adjust the iframe's height dynamically.
-                bounds={true}
-                onResize={rect => setHeight(rect.bounds.height)}
-              >
-                {({measureRef}) => <div ref={measureRef}>{children}</div>}
-              </Measure>
-            </StyleSheetManager>
-          )
-        }}
-      </FrameContextConsumer>
-    </FrameComponent>
+    // eslint-disable-next-line jsx-a11y/iframe-has-title
+    <iframe ref={setIframeRef} style={{width: '100%', border: 0, borderRadius: 6, height}}>
+      {
+        // By default, styled-components injects styles in the head of the page.
+        // However, styles from the page's head don't apply inside iframes.
+        // We're using StyleSheetManager to make styled-components inject styles
+        // into the head of the iframe instead.
+        (contentDocument !== null) && ReactDOM.createPortal(
+          <StyleSheetManager target={contentDocument.head}>
+            <Measure
+              // iframes don't adjust to the height of their content by default.
+              // We're using Measure to calculate the size of the content
+              // and adjust the iframe's height dynamically.
+              bounds={true}
+              onResize={rect => setHeight(rect.bounds.height)}
+            >
+              {({measureRef}) => <div ref={measureRef}>{children}</div>}
+            </Measure>
+          </StyleSheetManager>,
+          contentDocument.body
+        )
+      }
+    </iframe>
   )
 }
 
