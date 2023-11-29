3e8f3ad4740e739b53b11e1eaae2d8fced722999
Nicolas Solerieu <nsolerieu@users.noreply.github.com>
2023-11-29 11:54:42 -0800

    Update paragraph.js
    Increase the margin bottom of paragraphs

--- theme/src/components/paragraph.js
+++ theme/src/components/paragraph.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import themeGet from '@styled-system/theme-get'
 
 const Paragraph = styled.p`
-  margin: 0 0 ${themeGet('space.3')};
+  margin: 0 0 ${themeGet('space.4')};
 `
 
 export default Paragraph
