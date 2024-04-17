612f80d842c6ece23c86a2c11e969575fe8716d1
Daniel Adams <dipree@github.com>
2024-04-17 14:20:58 +0200

    Add tracking snippets

--- theme/src/components/head.js
+++ theme/src/components/head.js
@@ -15,6 +15,8 @@ function Head(props) {
       <meta property="og:description" content={description} />
       <meta property="og:image" content={siteMetadata.imageUrl} />
       <meta property="twitter:card" content="summary_large_image" />
+      <meta name="ha-url" content="https://collector.githubapp.com/primer/collect" />
+      <script src="https://analytics.githubassets.com/hydro-marketing.min.js"></script>
     </Helmet>
   )
 }
